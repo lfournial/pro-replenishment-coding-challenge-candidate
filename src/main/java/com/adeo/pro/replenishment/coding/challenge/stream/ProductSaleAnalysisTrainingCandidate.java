25f69e59babe187b3bfee7271916f32a8edf444d
Loic Fournial <loic.fournial@adeo.com>
2021-07-07 08:52:14 +0200

    1st attempt

--- src/main/java/com/adeo/pro/replenishment/coding/challenge/stream/ProductSaleAnalysisTrainingCandidate.java
+++ src/main/java/com/adeo/pro/replenishment/coding/challenge/stream/ProductSaleAnalysisTrainingCandidate.java
@@ -1,5 +1,9 @@
 package com.adeo.pro.replenishment.coding.challenge.stream;
 
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.reducing;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -9,6 +13,7 @@ import java.util.stream.Stream;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
 
 public class ProductSaleAnalysisTrainingCandidate {
 
@@ -26,8 +31,15 @@ public class ProductSaleAnalysisTrainingCandidate {
 		 */
 		System.out.println(" > Sorted by quantity (desc) for FR and IT BUs :");
 		
-		webClient.getProductSale().
-			forEach(System.out::println);
+//		webClient.getProductSale().
+//			forEach(System.out::println);
+		
+		webClient.getProductSale()
+        .filter(product -> (product.getBusinessUnit().equals(BusinessUnit.FR.name()) ||
+                product.getBusinessUnit().equals(BusinessUnit.IT.name())))
+                .sorted((product1, product2) -> product1.getQuantity().compareTo(product2.getQuantity()) * -1)
+        .forEach(System.out::println);
+
 		
 		System.out.println(" > -------------- ");
 		
@@ -39,9 +51,17 @@ public class ProductSaleAnalysisTrainingCandidate {
 		 */
 		System.out.println(" > Sorted by the total amount (desc) for all BUs :");
 		
-		webClient.getProductSale().
-			map(ProductSaleEnrichment::new).
-			forEach(System.out::println);
+//		webClient.getProductSale().
+//			map(ProductSaleEnrichment::new).
+//			forEach(System.out::println);
+		
+		webClient.getProductSale().map(ProductSaleEnrichment::new).map(
+                product -> {
+                    product.setTotalAmount(product.getPricePerUnit() * product.getQuantity());
+                    return product;
+                })
+                .sorted((product1, product2) -> product1.getTotalAmount().compareTo(product2.getTotalAmount()))
+                .forEach(System.out::println);
 		
 		System.out.println(" > -------------- ");
 		
@@ -52,8 +72,10 @@ public class ProductSaleAnalysisTrainingCandidate {
 		 */
 		System.out.println(" > The product with lowest sale (lowest quantity) for all BUs :");
 		
-		Optional<ProductSale> lowestSale = webClient.getProductSale().
-			findFirst();
+//		Optional<ProductSale> lowestSale = webClient.getProductSale().
+//			findFirst();
+        Optional<ProductSale> lowestSale = webClient.getProductSale()
+                .min((product1, product2) -> product1.getQuantity().compareTo(product2.getQuantity()));
 		
 		System.out.println(" > The product sale with the lowest sale is ".concat(lowestSale.get().toString()));
 		System.out.println(" > -------------- ");
@@ -65,9 +87,17 @@ public class ProductSaleAnalysisTrainingCandidate {
 		 */
 		System.out.println(" > The product with highest total amount for all BUs :");
 		
-		Optional<ProductSaleEnrichment> highestTotalAmount = webClient.getProductSale().
-			map(ProductSaleEnrichment::new).
-			findFirst();
+//		Optional<ProductSaleEnrichment> highestTotalAmount = webClient.getProductSale().
+//			map(ProductSaleEnrichment::new).
+//			findFirst();
+		
+		Optional<ProductSaleEnrichment> highestTotalAmount = webClient.getProductSale().map(ProductSaleEnrichment::new).map(
+                product -> {
+                    product.setTotalAmount(product.getPricePerUnit() * product.getQuantity());
+                    return product;
+                })
+                .max((product1, product2) -> product1.getTotalAmount().compareTo(product2.getTotalAmount()));
+		
 		
 		System.out.println(" > The product sale with the highest total amount is ".concat(highestTotalAmount.get().toString()));
 		System.out.println(" > -------------- ");
@@ -79,7 +109,32 @@ public class ProductSaleAnalysisTrainingCandidate {
 		 */
 		System.out.println(" > The product with highest total amount with an aggregation by product name :");
 		
-		webClient.getProductSale();
+//		webClient.getProductSale();
+
+// 		webClient.getProductSale().map(ProductSaleEnrichment::new).map(
+//              product -> {
+//                  product.setTotalAmount(product.getPricePerUnit() * product.getQuantity());
+//                  return product;
+//              })
+//              .collect(groupingBy(ProductSaleEnrichment::getName, TreeMap::new, summingDouble(ProductSaleEnrichment::getTotalAmount)))
+//              .entrySet().stream()
+//              .sorted(Map.Entry.comparingByValue())
+//              .forEach(product -> System.out.println(product.getKey() + "= " + product.getValue()));
+//      
+//      Mais on a pas le produit :-(
+
+		webClient.getProductSale().map(ProductSaleEnrichment::new).map(
+              product -> {
+                  product.setTotalAmount(product.getPricePerUnit() * product.getQuantity());
+                  return product;
+              })
+                .collect(groupingBy(ProductSaleEnrichment::getName, collectingAndThen(reducing(
+                        (product1, product2) -> new ProductSaleEnrichment(product1.getId(), product1.getName(), product1.getQuantity() + product2.getQuantity(), null, null,
+                                product1.getTotalAmount() + product2.getTotalAmount())),
+                      Optional<ProductSaleEnrichment>::get)))
+              .entrySet().stream()
+                .sorted((product1, product2) -> product1.getValue().getTotalAmount().compareTo(product2.getValue().getTotalAmount()) * -1)
+              .forEach(product -> System.out.println(product.getValue()));
 		
 		System.out.println(" > -------------- ");
 	}
@@ -136,6 +191,12 @@ public class ProductSaleAnalysisTrainingCandidate {
 		public ProductSaleEnrichment(ProductSale productSale) {
 			super(productSale.getId(), productSale.getName(), productSale.getQuantity(), productSale.getPricePerUnit(), productSale.getBusinessUnit());
 		}
+
+        public ProductSaleEnrichment(String id, String name, Integer quantity, Double pricePerUnit, String businessUnit, Double totalAmount) {
+            super(id, name, quantity, pricePerUnit, businessUnit);
+            this.totalAmount = totalAmount;
+        }
+
 	}
 	
 	public static enum BusinessUnit {
